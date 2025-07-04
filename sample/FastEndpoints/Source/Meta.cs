1db6a54bd954e62d0c30311ad578938ecc944527
Nacho Orlandoni <ignacio@softwareis.cool>
2025-07-04 01:47:43 -0400

    Added sample projects

---
+++ sample/FastEndpoints/Source/Meta.cs
@@ -0,0 +1,9 @@
+﻿global using FastEndpoints;
+global using FastEndpoints.Security;
+global using FastEndpoints.Swagger;
+global using FastEndpoints;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
+
+public partial class Program;
